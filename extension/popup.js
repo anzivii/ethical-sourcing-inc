4deaf03bc54146d90796f72510e1ebec1daee9c0
anzivii <alexar28@bss.on.ca>
2026-05-02 16:04:01 -0400

    icon test

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,9 @@
 document.getElementById('btn').addEventListener('click', () => {
   alert('Extension button clicked!');
 });
+fetch(chrome.runtime.getURL('brands.json'))
+  .then(res => res.json())
+  .then(brands => {
+    console.log("Database loaded!", brands);
+    // Add your scanning logic here
+  });
