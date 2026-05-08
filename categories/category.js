05324a4de2e9e3c00bb06719892704b41ba69dd6
aadya-01 <aadyar28@bss.on.ca>
2026-05-08 09:03:56 -0400

    changes

--- categories/category.js
+++ categories/category.js
@@ -1,5 +1,47 @@
 import { companies } from "../website files/dataset.js";
 
+const input = document.getElementById("searchInput");
+const suggestions = document.getElementById("suggestions");
+const resultContainer = document.getElementById("resultContainer");
+
+input.addEventListener("focus", showSuggestions);
+input.addEventListener("input", showSuggestions);
+
+function showSuggestions() {
+  const query = input.value.toLowerCase();
+
+  if (!query) {
+    suggestions.style.display = "none";
+    return;
+  }
+
+  suggestions.innerHTML = "";
+  suggestions.style.display = "block";
+
+  const filtered = companies.filter(c =>
+    c.name.toLowerCase().includes(query)
+  );
+
+  filtered.slice(0, 8).forEach(company => {
+    const div = document.createElement("div");
+    div.classList.add("suggestion-item");
+    div.textContent = company.name;
+
+    div.addEventListener("click", () => {
+      input.value = company.name;
+      suggestions.style.display = "none";
+
+      resultContainer.style.display = "block";
+      const alternatives = getAlternatives(company);
+    });
+
+    suggestions.appendChild(div);
+    const script = document.createElement("script");
+    script.src = "../website files/script.js";
+    document.body.appendChild(script);
+  });
+}
+
 const tableBody = document.getElementById("table-body");
 const title = document.getElementById("category-title");
 
